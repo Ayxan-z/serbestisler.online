b97e457c429215406a9209df0379b66d77ef17b9
= <=>
2021-09-10 19:37:36 +0400

    comment model created

---
+++ blog/models/comment.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from blog.models import ArticleModel
+
+class CommentModel(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    article = models.ForeignKey(ArticleModel, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    arrangement_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        db_table = 'Comment'
+    
