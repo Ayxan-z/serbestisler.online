365e4c14ad1ff15f6f8725156ed303907a77fb26
= <=>
2021-09-11 17:32:15 +0400

    fixed: nama_surname -->name_surname

--- blog/models/contact.py
+++ blog/models/contact.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ContactModel(models.Model):
     email = models.EmailField(max_length=250)
-    nama_surname = models.CharField(max_length=150)
+    name_surname = models.CharField(max_length=150)
     message = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
     
