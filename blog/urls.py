a9f3e341cb7dba8beb36ef17d0fd1cd0b5b13c09
= <=>
2021-09-13 00:09:50 +0400

    templatetags folder created

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from blog import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.homePage),
-    path('contact', views.contact),
+    path('', views.homePage, name='homepage'),
+    path('contact', views.contact, name='contact'),
 ]
