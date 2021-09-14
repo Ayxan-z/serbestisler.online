24fcf34f6b2bad8152e5839d2f54bcbaa8eed3fc
= <=>
2021-09-14 20:08:00 +0400

    .

--- blog/views/homepage.py
+++ blog/views/homepage.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from blog.models import ArticleModel
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def homePage(request):
+    search = request.GET.get('search')
     articles = ArticleModel.objects.order_by('-id')
+
+    if search:
+        articles = articles.filter(
+            Q(title__icontains=search) |
+            Q(content__icontains=search)
+        ).distinct()
+    
     page = request.GET.get('page')
     paginator = Paginator(articles, 3)
 
