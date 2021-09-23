c1a70ad6e8ae51e6bc1c6057ef4dfaada3b332de
= <=>
2021-09-23 17:27:56 +0400

    sentry added

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env()
 
@@ -135,3 +137,16 @@ LOGGING = {
     },
 }
 
+sentry_sdk.init(
+    dsn=env('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
