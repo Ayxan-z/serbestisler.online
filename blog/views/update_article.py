7b6dd1716cc215b05f51b7381d1267efb908cdea
= <=>
2021-10-04 23:20:34 +0400

    .

--- blog/views/update_article.py
+++ blog/views/update_article.py
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class UpdateArticleUpdateView(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
     template_name = 'pages/update-article.html'
-    fields = ('title', 'content', 'image', 'categories')
+    fields = ('title', 'content', 'files', 'categories')
     
     def get_object(self):
         article = get_object_or_404(ArticleModel,slug=self.kwargs.get('slug'),author=self.request.user)
