51e6f420e62fdb932ceabdd732ae5058e1799b1f
= <=>
2021-11-30 22:11:58 +0400

    .

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static/'
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['www.serbestisler.online', 'serbestisler.online', '127.0.0.1', '13.49.226.105']
+ALLOWED_HOSTS = ['serbestisler.online', '127.0.0.1', '16.170.218.199']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -40,14 +40,14 @@ DATABASES = {
 
 AWS_ACCESS_KEY_ID = env('ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'ayxanshahsuvarovs3'
+AWS_STORAGE_BUCKET_NAME = 'serbestisler-s3'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'config.storage_backend.MediaStorage'
 
