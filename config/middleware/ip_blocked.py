9e91524847c2cd66e019f58aa4257a6116bf1e1f
= <=>
2021-12-12 20:43:39 +0400

    .

--- config/middleware/ip_blocked.py
+++ config/middleware/ip_blocked.py
@@ -7,7 +7,14 @@ class BlockedIpMiddleware:
 
     def __call__(self, request):
         BLOCKED_IPS = ['46.228.176.95']
-        if request.META['REMOTE_ADDR'] in BLOCKED_IPS:
+        x_forw_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forw_for is not None:
+            ip = x_forw_for.split(',')[0]
+        
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        
+        if ip in BLOCKED_IPS: # request.META['REMOTE_ADDR']
             return http.HttpResponseForbidden('<h1>Forbidden</h1>')
 
         return self.get_response(request)
